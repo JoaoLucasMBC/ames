0a40cf878d57ac246974a7d4980999224e7d0b91
PedroPertusi <pedrovazpertusi@gmail.com>
2023-10-22 17:40:57 -0300

    api pronta

--- api/app.py
+++ api/app.py
@@ -1,8 +1,8 @@
-import csv
 from flask import Flask, request, jsonify
 import numpy as np
 import pandas as pd
 from feature_engeneering import feature_engeneering
+
 
 app = Flask(__name__)
 
@@ -10,29 +10,84 @@ app = Flask(__name__)
 @app.route('/api/predict', methods=['POST'])
 def predict_price():
     if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 400
+        return jsonify({"Mensagem": "Faltando JSON na requisição"}), 400
     
-    with open('colunas.txt', 'r') as file:
-        columns = [col.strip() for col in file.readlines()]
+    columns = pd.read_csv("most_occuring.csv").columns.tolist()
+        
 
     data = request.get_json(force=True)
     for col in data.keys():
         if col not in columns:
-            return jsonify({"msg": "Column {} does not exist".format(col)}), 400
+            return jsonify({"Mensagem": "Coluna {} não existe".format(col)}), 400
 
 
     data = request.get_json(force=True)
-    most_occ = pd.read_csv('most_occuring.csv')
 
-    # Create a dictionary with JSON data and for missing values grab the one from most_occ df
-    row_data = {col: data.get(col, most_occ[col].iloc[0]) for col in columns}
+    data_most = pd.read_csv('/Users/pedropertusi/Desktop/4o semestre/ML/ames/api/most_occuring.csv')
+
+    row_data = {}
+
+    for col in columns:
+        if col in data.keys():
+            row_data[col] = data[col]
+        else:
+            row_data[col] = data_most[col][0]
 
     # Create a DataFrame with one row
     df = pd.DataFrame([row_data])
+
+
+    # I need the Dtype to be the same as the original df
+
+    continuous_variables = ['Lot.Frontage','Lot.Area','Mas.Vnr.Area','BsmtFin.SF.1','BsmtFin.SF.2','Bsmt.Unf.SF','Total.Bsmt.SF','X1st.Flr.SF','X2nd.Flr.SF','Low.Qual.Fin.SF','Gr.Liv.Area','Garage.Area','Wood.Deck.SF','Open.Porch.SF','Enclosed.Porch','X3Ssn.Porch','Screen.Porch','Pool.Area','Misc.Val']
+
+    discrete_variables = ['Year.Built','Year.Remod.Add','Bsmt.Full.Bath','Bsmt.Half.Bath','Full.Bath','Half.Bath','Bedroom.AbvGr','Kitchen.AbvGr','TotRms.AbvGrd','Fireplaces','Garage.Yr.Blt','Garage.Cars','Mo.Sold','Yr.Sold',] 
+
+    categorical_variables = ['MS.SubClass','MS.Zoning','Street','Alley','Land.Contour','Lot.Config','Neighborhood','Condition.1','Condition.2','Bldg.Type','House.Style','Roof.Style','Roof.Matl','Exterior.1st','Exterior.2nd','Mas.Vnr.Type','Foundation','Heating','Central.Air','Garage.Type','Misc.Feature','Sale.Type','Sale.Condition',] 
+
+    for col in continuous_variables:
+        df[col] = df[col].astype('float64')
+
+    for col in categorical_variables:
+        df[col] = df[col].astype('category')
+
+    for col in discrete_variables:
+        df[col] = df[col].astype('float64')
+
+    category_orderings = {
+    'Lot.Shape': ['Reg','IR1','IR2','IR3',],
+    'Utilities': ['AllPub','NoSewr','NoSeWa','ELO',],
+    'Land.Slope': ['Gtl','Mod','Sev',],
+    'Overall.Qual': [1,2,3,4,5,6,7,8,9,10,],
+    'Overall.Cond': [1,2,3,4,5,6,7,8,9,10,],
+    'Exter.Qual': ['Ex','Gd','TA','Fa','Po',],
+    'Exter.Cond': ['Ex','Gd','TA','Fa','Po',],
+    'Bsmt.Qual': ['Ex','Gd','TA','Fa','Po',],
+    'Bsmt.Cond': ['Ex','Gd','TA','Fa','Po',],
+    'Bsmt.Exposure': ['Gd','Av','Mn','No','NA',],
+    'BsmtFin.Type.1': ['GLQ','ALQ','BLQ','Rec','LwQ','Unf',],
+    'BsmtFin.Type.2': ['GLQ','ALQ','BLQ','Rec','LwQ','Unf',],
+    'Heating.QC': ['Ex','Gd','TA','Fa','Po',],
+    'Electrical': ['SBrkr','FuseA','FuseF','FuseP','Mix',],
+    'Kitchen.Qual': ['Ex','Gd','TA','Fa','Po',],
+    'Functional': ['Typ','Min1','Min2','Mod','Maj1','Maj2','Sev','Sal',],
+    'Fireplace.Qu': ['Ex','Gd','TA','Fa','Po',],
+    'Garage.Finish': ['Fin','RFn','Unf',],
+    'Garage.Qual': ['Ex','Gd','TA','Fa','Po',],
+    'Garage.Cond': ['Ex','Gd','TA','Fa','Po',],
+    'Paved.Drive': ['Y','P','N',],
+    'Pool.QC': ['Ex','Gd','TA','Fa',],
+    'Fence': ['GdPrv','MnPrv','GdWo','MnWw',],
+}
     
-    print(df.shape)
+    for col, orderings in category_orderings.items():
+        df[col] = df[col].astype('category').cat.set_categories(orderings, ordered=True)
+
+
+
+    
     x = feature_engeneering(df)
-    return jsonify({"msg": "Success", "data": x}), 200
+    return jsonify({"Mensagem": "O valor do imóvel foi predito com sucesso!", "Valor Predito": f"${x:.2f}"}), 200
 
 
 
--- api/feature_engeneering.py
+++ api/feature_engeneering.py
@@ -4,118 +4,266 @@ from sklearn.preprocessing import StandardScaler, RobustScaler
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.ensemble import GradientBoostingRegressor
+import pickle
+import pathlib
 
 RANDOM_SEED = 42
+
+def remap_categories(
+    series: pd.Series,
+    old_categories: tuple[str],
+    new_category: str,
+) -> pd.Series:
+    # Add the new category to the list of valid categories.
+    series = series.cat.add_categories(new_category)
+
+    # Set all items of the old categories as the new category.
+    remapped_items = series.isin(old_categories)
+    series.loc[remapped_items] = new_category
+
+    # Clean up the list of categories, the old categories no longer exist.
+    series = series.cat.remove_unused_categories()
+
+    return series
 
 def feature_engeneering(df_novo):
 
     X = df_novo.copy()
+
+    selection = ~(X['MS.Zoning'].isin(['A (agr)', 'C (all)', 'I (all)']))
+    X = X[selection]
+
+    X['MS.Zoning'] = X['MS.Zoning'].cat.remove_unused_categories()
+
+    processed_data = X.copy()
+
+    processed_data['Sale.Type'] = remap_categories(series=processed_data['Sale.Type'],old_categories=('WD ', 'CWD', 'VWD'),new_category='GroupedWD',)
+    processed_data['Sale.Type'] = remap_categories(series=processed_data['Sale.Type'],old_categories=('COD', 'ConLI', 'Con', 'ConLD', 'Oth', 'ConLw'),new_category='Other',)
+
+    X = processed_data
+
+    X = X.drop(columns='Street')
+
+    processed_data = X.copy()
+
+    for col in ('Condition.1', 'Condition.2'):
+        processed_data[col] = remap_categories(series=processed_data[col],old_categories=('RRAn', 'RRAe', 'RRNn', 'RRNe'),new_category='Railroad',)
+        processed_data[col] = remap_categories(series=processed_data[col],old_categories=('Feedr', 'Artery'),new_category='Roads',)
+        processed_data[col] = remap_categories(series=processed_data[col],old_categories=('PosA', 'PosN'),new_category='Positive',)
+        
+    processed_data['Condition'] = pd.Series(index=processed_data.index,dtype=pd.CategoricalDtype(categories=('Norm','Railroad','Roads','Positive','RoadsAndRailroad',)),)
+    
+    norm_items = processed_data['Condition.1'] == 'Norm'
+    processed_data['Condition'][norm_items] = 'Norm'
+
+    railroad_items = (processed_data['Condition.1'] == 'Railroad') & (processed_data['Condition.2'] == 'Norm')
+    processed_data['Condition'][railroad_items] = 'Railroad'
+
+    roads_items = (processed_data['Condition.1'] == 'Roads') & (processed_data['Condition.2'] != 'Railroad')
+    processed_data['Condition'][roads_items] = 'Roads'
+
+    positive_items = processed_data['Condition.1'] == 'Positive'
+    processed_data['Condition'][positive_items] = 'Positive'
+
+    roads_and_railroad_items = ((processed_data['Condition.1'] == 'Railroad') & (processed_data['Condition.2'] == 'Roads')) | ( (processed_data['Condition.1'] == 'Roads') & (processed_data['Condition.2'] == 'Railroad'))
+    processed_data['Condition'][roads_and_railroad_items] = 'RoadsAndRailroad'
+    processed_data = processed_data.drop(columns=['Condition.1', 'Condition.2'])
+
+    X = processed_data
+
+    X['HasShed'] = X['Misc.Feature'] == 'Shed'
+    X = X.drop(columns='Misc.Feature')
+
+    X['HasAlley'] = ~X['Alley'].isna()
+    X = X.drop(columns='Alley')
+
+    X['Exterior.2nd'] = remap_categories(series=X['Exterior.2nd'],old_categories=('Brk Cmn', ),new_category='BrkComm',)
+    X['Exterior.2nd'] = remap_categories(series=X['Exterior.2nd'],old_categories=('CmentBd', ),new_category='CemntBd',)
+    X['Exterior.2nd'] = remap_categories(series=X['Exterior.2nd'],old_categories=('Wd Shng', ),new_category='WdShing',)
+
+    for col in ('Exterior.1st', 'Exterior.2nd'):
+        categories = X[col].cat.categories
+        X[col] = X[col].cat.reorder_categories(sorted(categories))
+
+    processed_data = X.copy()
+
+    mat_count = processed_data['Exterior.1st'].value_counts()
+    rare_materials = list(mat_count[mat_count < 40].index)
+
+    processed_data['Exterior'] = remap_categories(series=processed_data['Exterior.1st'],old_categories=rare_materials,new_category='Other',)
+
+    processed_data = processed_data.drop(columns=['Exterior.1st', 'Exterior.2nd'])
+
+    X = processed_data
+
+    X = X.drop(columns='Heating')
+
+    X = X.drop(columns='Roof.Matl')
+
+    X['Roof.Style'] = remap_categories(series=X['Roof.Style'],old_categories=['Flat','Gambrel','Mansard','Shed',],new_category='Other',)
+
+    X['Mas.Vnr.Type'] = remap_categories(series=X['Mas.Vnr.Type'],old_categories=['BrkCmn','CBlock',],new_category='Other',)
+
+    X['Mas.Vnr.Type'] = X['Mas.Vnr.Type'].cat.add_categories('None')
+
+    X['MS.SubClass'] = remap_categories(series=X['MS.SubClass'],old_categories=[75, 45, 180, 40, 150],new_category='Other',)
+
+    X['Foundation'] = remap_categories(series=X['Foundation'],old_categories=['Slab', 'Stone', 'Wood'],new_category='Other',)
+
+    selection = ~X['Neighborhood'].isin(['Blueste','Greens','GrnHill','Landmrk',])
+    X = X[selection]
+
+    X['Neighborhood'] = X['Neighborhood'].cat.remove_unused_categories()
+
+    X['Garage.Type'] = X['Garage.Type'].cat.add_categories(['NoGarage'])
+
+    X = X.drop(columns='Utilities')
+    X = X.drop(columns='Pool.QC')
+
+    old_categories = list(X['Fence'].cat.categories)
+    new_categories = old_categories + ['NoFence']
+    X['Fence'] = X['Fence'].cat.set_categories(new_categories)
+
+    X = X.drop(columns='Fireplace.Qu')
+
+    X = X.drop(columns=['Garage.Cond', 'Garage.Qual'])
+
+    X['Garage.Finish'] = X['Garage.Finish'].cat.as_unordered().cat.add_categories(['NoGarage'])
+
+    X['Electrical'][X['Electrical'].isna()] = 'SBrkr'
+
+    X['Bsmt.Exposure'][X['Bsmt.Exposure'].isna()] = 'NA'
+    X['Bsmt.Exposure'] = X['Bsmt.Exposure'].cat.as_unordered().cat.remove_unused_categories()
+
+    for col in ('Bsmt.Qual', 'Bsmt.Cond', 'BsmtFin.Type.1', 'BsmtFin.Type.2'):
+        X[col] = X[col].cat.add_categories(['NA'])
+        X[col][X[col].isna()] = 'NA'
+        X[col] = X[col].cat.as_unordered().cat.remove_unused_categories()
+
+    X['Bsmt.Cond'][X['Bsmt.Cond'] == 'Po'] = 'Fa'
+    X['Bsmt.Cond'][X['Bsmt.Cond'] == 'Ex'] = 'Gd'
+    X['Bsmt.Cond'] = X['Bsmt.Cond'].cat.remove_unused_categories()
+
+    garage_age = X['Yr.Sold'] - X['Garage.Yr.Blt'] 
+    garage_age[garage_age < 0.0] = 0.0
+    X = X.drop(columns='Garage.Yr.Blt')
+    X['Garage.Age'] = garage_age
+
+    remod_age = X['Yr.Sold'] - X['Year.Remod.Add']
+    remod_age[remod_age < 0.0] = 0.0
+
+    house_age = X['Yr.Sold'] - X['Year.Built']
+    house_age[house_age < 0.0] = 0.0
+
+    X = X.drop(columns=['Year.Remod.Add', 'Year.Built'])
+    X['Remod.Age'] = remod_age
+    X['House.Age'] = house_age
+
+    for col in X.select_dtypes('category').columns:
+        X[col] = X[col].cat.remove_unused_categories()
+
     for col in X.select_dtypes('category'):
         X[col] = X[col].astype(object)
+
+    numeric_cols = X.select_dtypes(include=[np.number]).columns
 
     X['MS.SubClass'] = X['MS.SubClass'].apply(lambda x: str(x))
     X['Mo.Sold'] = X['Mo.Sold'].apply(lambda x: str(x))
     X['Yr.Sold'] = X['Yr.Sold'].apply(lambda x: str(x))
 
-
-
     X['Lot.Shape'].replace({'Reg': 1, 'IR1': 2, 'IR2': 3, 'IR3': 4}, inplace=True)
 
     X['Land.Contour'].replace({'Low':1, 'HLS':2, 'Bnk':3, 'Lvl':4}, inplace=True)
-
     X['Land.Slope'].replace({'Sev':1, 'Mod':2, 'Gtl':3}, inplace=True)
-
     X['Exter.Qual'].replace({'Po':1, 'Fa':2, 'TA':3, 'Gd':4, 'Ex':5}, inplace=True)
-
     X['Exter.Cond'].replace({'Po':1, 'Fa':2, 'TA':3, 'Gd':4, 'Ex':5}, inplace=True)
-
     X['Bsmt.Qual'].replace({'NA':0, 'Po':1, 'Fa':2, 'TA':3, 'Gd':4, 'Ex':5}, inplace=True)
-
     X['Bsmt.Cond'].replace({'NA':0, 'Po':1, 'Fa':2, 'TA':3, 'Gd':4, 'Ex':5}, inplace=True)
-
     X['Bsmt.Exposure'].replace({'NA':0, 'No':1, 'Mn':2, 'Av':3, 'Gd':4}, inplace=True)
-
     X['Heating.QC'].replace({'Po':1, 'Fa':2, 'TA':3, 'Gd':4, 'Ex':5}, inplace=True)
-
     X['Kitchen.Qual'].replace({'Po':1, 'Fa':2, 'TA':3, 'Gd':4, 'Ex':5}, inplace=True)
-
     X['Functional'].replace({'Sal':1, 'Sev':2, 'Maj2':3, 'Maj1':4, 'Mod':5, 'Min2':6, 'Min1':7, 'Typ':8}, inplace=True)
-
     X['Garage.Finish'].replace({'NoGarage':0, 'Unf':1, 'RFn':2, 'Fin':3}, inplace=True)
-
     X['Paved.Drive'].replace({'N':1, 'P':2, 'Y':3}, inplace=True)
 
-    X.loc[(X['Mas.Vnr.Type'] == 'None') & (X['Mas.Vnr.Area'] > 1), 'Mas.Vnr.Type'] = 'BrkFace' # most common 
-    X.loc[(X['Mas.Vnr.Type'] == 'None') & (X['Mas.Vnr.Area'] == 1), 'Mas.Vnr.Area'] = 0 # 1 sq ft is basically 0
+    numeric_cols = X.select_dtypes(include=[np.number]).columns
+
+    X.loc[(X['Mas.Vnr.Type'] == 'None') & (X['Mas.Vnr.Area'] > 1), 'Mas.Vnr.Type'] = 'BrkFace'
+    X.loc[(X['Mas.Vnr.Type'] == 'None') & (X['Mas.Vnr.Area'] == 1), 'Mas.Vnr.Area'] = 0 
     for vnr_type in X['Mas.Vnr.Type'].unique():
-        # so here we set the area equal to the mean of the given veneer type
         X.loc[(X['Mas.Vnr.Type'] == vnr_type) & (X['Mas.Vnr.Area'] == 0), 'Mas.Vnr.Area'] = \
             X[X['Mas.Vnr.Type'] == vnr_type]['Mas.Vnr.Area'].mean() 
-
-
-    idx_to_drop = X[X['Gr.Liv.Area'] >= 4000].index
-    X.drop(idx_to_drop, inplace=True)
-
-    # Total Square Footage
+    
     X['Total.SF'] = X['Total.Bsmt.SF'] + X['Gr.Liv.Area']
     X['Total.Floor.SF'] = X['X1st.Flr.SF'] + X['X2nd.Flr.SF']
     X['Total.Porch.SF'] = X['Open.Porch.SF'] + X['Enclosed.Porch'] + \
         X['X3Ssn.Porch'] + X['Screen.Porch']
         
-    # Total Bathrooms
     X['Total.Bathrooms'] = X['Full.Bath'] + .5 * X['Half.Bath'] + \
         X['Bsmt.Full.Bath'] + .5 * X['Bsmt.Half.Bath']
 
-    # Booleans
     X['Has.Basement'] = X['Total.Bsmt.SF'].apply(lambda x: 1 if x > 0 else 0)
     X['Has.Garage'] = X['Garage.Area'].apply(lambda x: 1 if x > 0 else 0)
     X['Has.Porch'] = X['Total.Porch.SF'].apply(lambda x: 1 if x > 0 else 0)
     X['Has.Pool'] = X['Pool.Area'].apply(lambda x: 1 if x > 0 else 0)
     X['Was.Completed'] = (X['Sale.Condition'] != 'Partial').astype(np.int64)
 
-    
-    boolean_features = ['Has.Basement', 'Has.Garage', 'Has.Porch', 'Has.Pool', 'Was.Completed']
-
     numeric_cols = X.select_dtypes(include=[np.number]).columns
-    numeric_cols = [f for f in numeric_cols if f not in boolean_features]
     cat_cols = X.select_dtypes(include=['object']).columns
 
-    skew_limit = 0.5
-    skew_vals = X[numeric_cols].skew()
-
-    high_skew = skew_vals[abs(skew_vals) > skew_limit]
-    skew_cols = high_skew.index.tolist()
-
-    for col in skew_cols:
+    for col in ['Lot.Frontage', 'Lot.Area', 'Lot.Shape', 'Land.Contour', 'Land.Slope', 'Mas.Vnr.Area', 'Exter.Qual', 'Exter.Cond', 'Bsmt.Qual', 'Bsmt.Cond', 'Bsmt.Exposure', 'BsmtFin.SF.1', 'BsmtFin.SF.2', 'Bsmt.Unf.SF', 'Heating.QC', 'X1st.Flr.SF', 'X2nd.Flr.SF', 'Low.Qual.Fin.SF', 'Gr.Liv.Area', 'Bsmt.Full.Bath', 'Bsmt.Half.Bath', 'Half.Bath', 'Kitchen.AbvGr', 'TotRms.AbvGrd', 'Functional', 'Fireplaces', 'Paved.Drive', 'Wood.Deck.SF', 'Open.Porch.SF', 'Enclosed.Porch', 'X3Ssn.Porch', 'Screen.Porch', 'Pool.Area', 'Misc.Val', 'Garage.Age', 'House.Age', 'Total.SF', 'Total.Floor.SF', 'Total.Porch.SF']:
         X[col] = np.log1p(X[col])
         X[col] = X[col].astype(np.float64)
 
-    X['Overall.Qual'] = X['Overall.Qual'].astype(np.int64)
-    X['Overall.Cond'] = X['Overall.Cond'].astype(np.int64)
+    X['Lot.Shape'] = X['Lot.Shape'].apply(lambda x: float(x))
+    X['Land.Contour'] = X['Land.Contour'].apply(lambda x: float(x))
+    X['Land.Slope'] = X['Land.Slope'].apply(lambda x: float(x))
+    X['Exter.Qual'] = X['Exter.Qual'].apply(lambda x: float(x))
+    X['Exter.Cond'] = X['Exter.Cond'].apply(lambda x: float(x))
+    X['Bsmt.Qual'] = X['Bsmt.Qual'].apply(lambda x: float(x))
+    X['Bsmt.Cond'] = X['Bsmt.Cond'].apply(lambda x: float(x))
+    X['Bsmt.Exposure'] = X['Bsmt.Exposure'].apply(lambda x: float(x))
+    X['Heating.QC'] = X['Heating.QC'].apply(lambda x: float(x))
+    X['Functional'] = X['Functional'].apply(lambda x: float(x))
+    X['Paved.Drive'] = X['Paved.Drive'].apply(lambda x: float(x))
+    X['Mo.Sold'] = X['Mo.Sold'].apply(lambda x: float(x))
+    X['Yr.Sold'] = X['Yr.Sold'].apply(lambda x: float(x))
 
+    if X['Overall.Qual'].isna().any():
+        X['Overall.Qual'][0] = 5.0
+    if X['Overall.Cond'].isna().any():
+        X['Overall.Cond'][0] = 5.0
+
+    X['Overall.Qual'] = X['Overall.Qual'].astype('int64')
+    X['Overall.Cond'] = X['Overall.Cond'].astype('int64')
+
+    boolean_features = ['Has.Basement', 'Has.Garage', 'Has.Porch', 'Has.Pool', 'Was.Completed']
     numeric_cols = X.select_dtypes(include=[np.number]).columns
     numeric_cols = [f for f in numeric_cols if f not in boolean_features]
 
-    cat_cols = X.select_dtypes(include=['object']).columns
+    X_velho = pd.read_csv('/Users/pedropertusi/Desktop/4o semestre/ML/ames/api/x_model.csv')
+    y = pd.read_csv('/Users/pedropertusi/Desktop/4o semestre/ML/ames/api/y.csv')
+
+    X_velho = pd.get_dummies(X_velho, drop_first=True)
+    X = pd.get_dummies(X, drop_first=True)
+
+    #adicionar em X as colunas que não existem em X_velho e ordene as colunas na mesma ordem de X_velho
+    for col in X_velho.columns:
+        if col not in X.columns:
+            X[col] = 0
     
-    X_novo = pd.get_dummies(X, drop_first=True).copy()
-
-    # a partir daqui usar usar os dados origionais para treinar o modelo
-
-    X_model = pd.read_csv('x_model.csv')
-    y = pd.read_csv('y.csv')
-
-    X_train, X_test, y_train, y_test = train_test_split(X_model, y, test_size=0.2, random_state=RANDOM_SEED)
+    X = X[X_velho.columns]
 
     scaler = RobustScaler()
-    X_train.loc[:,numeric_cols] = scaler.fit_transform(X_train[numeric_cols])
-    X_test.loc[:,numeric_cols] = scaler.transform(X_test[numeric_cols])
-    X_novo.loc[:,numeric_cols] = scaler.transform(X_novo[numeric_cols])
+    X_velho.loc[:,numeric_cols] = scaler.fit_transform(X_velho[numeric_cols])
+    X.loc[:,numeric_cols] = scaler.transform(X[numeric_cols])
 
-    params = {'alpha': 6e-05, 'copy_X': True, 'fit_intercept': True, 'max_iter': 50000, 'positive': False, 'precompute': False, 'random_state': None, 'selection': 'cyclic', 'tol': 0.0001, 'warm_start': False}
+    lasso_params = {'alpha': 6e-05, 'copy_X': True, 'fit_intercept': True, 'max_iter': 50000, 'positive': False, 'precompute': False, 'random_state': None, 'selection': 'cyclic', 'tol': 0.0001, 'warm_start': False}
 
-    lasso = Lasso(**params)
-    lasso.fit(X_train, y_train)
+    lasso = Lasso(**lasso_params)
+    lasso.fit(X_velho, y)
+    result = lasso.predict(X)
 
-    y_pred = lasso.predict(X_novo)
-    return y_pred
+    
+
+    return 10**result[0]
